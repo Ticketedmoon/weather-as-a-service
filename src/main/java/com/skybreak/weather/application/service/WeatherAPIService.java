c98de10c85aa549ab480dec01c2f5bbb9d127828
Shane Creedon <shane.creedon3@mail.dcu.ie>
2021-05-14 00:28:36 +0100

    master: - Remove unncessary README.md - Add Rest config with RestTemplate bean - Change Weather API third-party - Introduce WeatherDTO
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- src/main/java/com/skybreak/weather/application/service/WeatherAPIService.java
+++ src/main/java/com/skybreak/weather/application/service/WeatherAPIService.java
@@ -12,17 +12,17 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class WeatherAPIService {
 
-    private static final String WEATHER_API_BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
-    private static final String WEATHER_API_CITY_QUERY_URL = WEATHER_API_BASE_URL + "?q=%s&appid=%s";
+    private static final String WEATHER_API_BASE_URL = "http://api.weatherstack.com";
+    private static final String WEATHER_API_CITY_QUERY_URL = WEATHER_API_BASE_URL + "/current?access_key=%s&query=%s";
 
-    @Value("weather-api-key")
-    private final String weatherApikey;
+    @Value("${weather-api-access-key}")
+    private String weatherApikey;
 
     private final RestTemplate restTemplate;
 
     public WeatherDTO getCurrentWeatherByCity(String city) {
         try {
-            return restTemplate.getForObject(String.format(WEATHER_API_CITY_QUERY_URL, city, weatherApikey), WeatherDTO.class);
+            return restTemplate.getForObject(String.format(WEATHER_API_CITY_QUERY_URL, weatherApikey, city), WeatherDTO.class);
         } catch (RestClientException e) {
             throw new WeatherAPIException("Error when making API request - Please verify city name", e);
         }
--- src/main/java/com/skybreak/weather/domain/model/WeatherDTO.java
+++ src/main/java/com/skybreak/weather/domain/model/WeatherDTO.java
@@ -1,5 +1,32 @@
 package com.skybreak.weather.domain.model;
 
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class WeatherDTO {
 
+    private WeatherDetails current;
+
+    @Getter
+    @Setter
+    private static class WeatherDetails {
+        private String observation_time;
+        private Integer temperature;
+        private List<String> weather_icons;
+        private List<String> weather_descriptions;
+        private Integer wind_speed;
+        private Integer wind_degree;
+        private String wind_dir;
+        private Integer pressure;
+        private Integer precip;
+        private Integer humidity;
+        private Integer cloudcover;
+        private Integer feelslike;
+        private Integer uv_index;
+        private Integer visibility;
+        private String is_day;
+    }
 }
