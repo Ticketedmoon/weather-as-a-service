acc610899c1311a0056c313948f80606fc5a460f
Shane Creedon <shane.creedon3@mail.dcu.ie>
2021-05-15 14:17:18 +0100

    master: - Initialise front-end - Add webpack, material and toastify - set up dev server
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- src/main/resources/static/react/src/components/app.tsx
+++ src/main/resources/static/react/src/components/app.tsx
@@ -1,3 +1,46 @@
+import React, {useState} from "react";
+import axios from "axios"
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import {TextField} from "@material-ui/core";
+
 export const App = () => {
-	return <p> NOT_IMPLEMENTED </p>
+
+	const [city, setCity] = useState<string>()
+
+	const getWeatherByCity = (city: string) => {
+		console.log(window.location.origin + "/api/weather");
+		axios.get(window.location.origin + "/api/weather", {
+			params: {
+				"city": city
+			}
+		}).then(res => {
+			console.log(res);
+		}).catch(err => {
+			toast.error('⚠ Something went wrong when checking the weather', {
+				position: "top-right",
+				autoClose: 3000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+			});
+		})
+	}
+
+	return (
+		<div>
+			<Autocomplete
+				id="cities-dropdown"
+				options={["Dublin", "Paris", "Vancouver", "Tokyo"]}
+				getOptionLabel={(option) => option}
+				onInputChange={(event, selectedCity: string) => setCity(selectedCity)}
+				style={{width: 300}}
+				renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined"/>}
+			/>
+			<button onClick={() => getWeatherByCity(city)}> Get weather </button>
+		</div>
+	)
 }
---
+++ src/main/resources/static/react/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+
+module.exports = (mode) => {
+    return {
+        mode: mode || "development",
+        entry: './src/index.tsx',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devtool: (mode === "development" || mode === "dev") ? 'source-map' : false,
+        devServer: {
+            host: 'localhost',
+            publicPath: 'http://localhost:3000/dist',
+            port: 3000,
+            compress: true,
+            contentBase: path.join(__dirname, "dist"),
+            watchContentBase: true,
+            writeToDisk: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: 'index.html',
+                inject: true
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/i,
+                    loader: 'ts-loader',
+                    exclude: ['/node_modules/'],
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        }
+    }
+};
